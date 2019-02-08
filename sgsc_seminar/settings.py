acc0018eacffdc86996e019b38d875b7972a4657
hyun-park <rockethyun@gmail.com>
2019-02-09 02:04:15 +0900

    whitenose changed

--- sgsc_seminar/settings.py
+++ sgsc_seminar/settings.py
@@ -85,14 +85,14 @@ WSGI_APPLICATION = 'sgsc_seminar.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        # 'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        # 'NAME': 'djangogirls',
-        # 'USER': 'name',
-        # 'PASSWORD': '',
-        # 'HOST': 'localhost',
-        # 'PORT': '',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'djangogirls',
+        'USER': 'name',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
