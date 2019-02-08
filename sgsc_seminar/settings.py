ba96ec2568498b48a0a08e3e46dfbf80abb9c88d
hyun-park <rockethyun@gmail.com>
2019-02-09 02:09:53 +0900

    pg setting

--- sgsc_seminar/settings.py
+++ sgsc_seminar/settings.py
@@ -83,18 +83,13 @@ WSGI_APPLICATION = 'sgsc_seminar.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
+# DATABASES = {
+    # 'default': {
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'djangogirls',
-        'USER': 'name',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '',
-    }
-}
+    # }
+    # 'default': dj_database_url.config()
+# }
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
