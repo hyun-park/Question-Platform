b81798a67f6a186a4a303bcbcb089077fac0cdb9
hyun-park <rockethyun@gmail.com>
2019-02-09 12:34:38 +0900

    question order changed

--- questions/views.py
+++ questions/views.py
@@ -23,7 +23,7 @@ def question_list(request):
         else:
             return redirect('question_list')
     else:
-        questions = Question.objects.annotate(upvote_counts=Count('upvotes')).order_by('-upvote_counts')
+        questions = Question.objects.annotate(upvote_counts=Count('upvotes')).order_by('-upvote_counts').order_by('-created_at')
         user_id = request.user.id
         for question in questions:
             question.isLiked = question.isLiked(user_id)
